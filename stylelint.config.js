14658014864add6cd112ba70baf57db4ded684c5
izhaoqing <zhaoq0104@163.com>
2022-05-12 15:02:46 +0800

    feat(stylelint): 增加规则忽略 windicss vue 特殊写法

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,14 @@
 module.exports = {
     extends: '@trscd/stylelint-config-tpconfig',
     ignoreFiles: ['./public/**/*'],
+    rules: {
+        // https://github.com/vuejs/rfcs/blob/master/active-rfcs/0023-scoped-styles-changes.md
+        'selector-pseudo-class-no-unknown': [true, { ignorePseudoClasses: ['deep'] }],
+        'selector-pseudo-element-no-unknown': [true, { ignorePseudoElements: ['v-deep'] }],
+        // https://windicss.org/features/directives.html
+        'at-rule-no-unknown': [true, { ignoreAtRules: ['apply', 'variants', 'screen', 'layer'] }],
+        // https://windicss.org/features/directives.html#theme
+        'function-no-unknown': [true, { ignoreFunctions: ['theme'] }],
+        'declaration-no-important': null,
+    },
 };
