64efe6e88e8bdaee858077260b4fe18ac97b1176
zhaoqing <zhinching@gmail.com>
2023-12-04 09:33:39 +0800

    build: 打印版本号

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import pkg from '../package.json';
 import router from './router';
 import App from './App.vue';
 import axios from '@/plugins/axios';
@@ -24,3 +25,15 @@ const app = createApp(App)
     .use(axios);
 directive(app);
 app.mount('#app');
+
+if (pkg.tag) {
+    const script = document.createElement('script');
+    script.innerHTML = `
+        console.log(
+            ' %c 当前版本：%c ${pkg.tag} ',
+            'background: #606060; color: #fff; border-radius: 3px 0 0 3px;',
+            'background: #1475B2; color: #fff; border-radius: 0 3px 3px 0;'
+        );
+    `;
+    document.body.appendChild(script);
+}
