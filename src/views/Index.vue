c5128627506f3bb963571fd54cba235dafd99d65
zhaoqing <zhinching@gmail.com>
2024-03-28 10:37:10 +0800

    feat: 增加大屏锁放组件

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,14 +1,16 @@
 <template>
-    <vue-logo></vue-logo>
-    <info-text></info-text>
-    <div class="flex justify-center mt-30px items-center space-x-20px">
-        <icons-vite></icons-vite>
-        <icons-antdv width="30px"></icons-antdv>
-        <icons-axios class="w-146px"></icons-axios>
-        <icons-unocss class="w-50px h-28px"></icons-unocss>
-        <icons-pinia class="h-30px w-auto"></icons-pinia>
-        <icons-pnpm class="h-26px"></icons-pnpm>
-        <icons-vueuse class="h-28px w-auto"></icons-vueuse>
+    <div class="overflow-hidden">
+        <vue-logo></vue-logo>
+        <info-text></info-text>
+        <div class="flex justify-center mt-30px items-center space-x-20px">
+            <icons-vite></icons-vite>
+            <icons-antdv width="30px"></icons-antdv>
+            <icons-axios class="w-146px"></icons-axios>
+            <icons-unocss class="w-50px h-28px"></icons-unocss>
+            <icons-pinia class="h-30px w-auto"></icons-pinia>
+            <icons-pnpm class="h-26px"></icons-pnpm>
+            <icons-vueuse class="h-28px w-auto"></icons-vueuse>
+        </div>
     </div>
 </template>
 
